a1d238776d868be850b61d739663df0e56d16025
Coder_Dheeraj/ <113779503+Dheeraj475@users.noreply.github.com>
2024-12-02 20:07:36 +0530

    changes of smax array

---
+++ GFG_DSA/src/com/dsa/D11_SmallestPositiveMissingNumber.java
@@ -0,0 +1,23 @@
+package com.dsa;
+
+public class D11_SmallestPositiveMissingNumber {
+
+	public static void main(String[] args) {
+		
+		int arr [] = {2, -3, 4, 1, 1, 7};
+
+	}
+
+	public int missingNumber(int[] arr) {
+		
+		for (int i = 1; i < arr.length; i++) {
+			
+			
+			
+		}
+		
+		return -1;
+
+	}
+
+}
--- GFG_DSA/src/com/dsa/D1_SecondLargestArray.java
+++ GFG_DSA/src/com/dsa/D1_SecondLargestArray.java
@@ -1,14 +1,72 @@
 package com.dsa;
 
-public class D1_SecondLargestArray {
+import java.util.Arrays;
 
+public class D1_SecondLargestArray {
+	
+	/*Finding max number*/ 
+	
+	//Brute force approach { TC -> O(n log n) }
+	public int largest(int[] arr) {
+		
+		Arrays.sort(arr);
+		int n = arr.length-1;
+		
+		int max = arr[n];
+		
+		return max;
+	}
+	
+	//Optimal approach { TC -> O(n) }
+	public int largest1(int[] arr) {
+		
+		int max = arr[0];
+		
+		for (int i = 1; i < arr.length; i++) {
+			
+			if (arr[i] > max) {
+				max = arr[i];
+			}
+			
+		}
+		
+		return max;
+		
+	}
+	
+	/*Finding second max number*/
+	
+	//Brute force approach { TC -> O(n log n) }
 	public int getSecondLargest(int[] arr) {
+		
+		Arrays.sort(arr);
+		
+		int n = arr.length-1;
+		int max = arr[n];
+		int max1 = Integer.MIN_VALUE;
+		
+		for(int i = n-1; i >= 0; i--) {
+			
+			if(arr[i] != max) {
+				max1 = arr[i];
+				break;
+			}
+			
+		}
+		
+		return max1;
+				
+	}
+	
+	
+	//Better approach { TC -> O(2n) }
+	public int getSecondLargest1(int[] arr) {
 
 		int max = arr[0];
 		int max1 = Integer.MIN_VALUE;
 		boolean check = true;
 
-		for (int i = 0; i < arr.length; i++) {
+		for (int i = 1; i < arr.length; i++) {
 
 			if (max < arr[i]) {
 				max = arr[i];
@@ -30,14 +88,46 @@ public class D1_SecondLargestArray {
 		
 		return max1;
 	}
+	
+	//Optimal approach { TC -> O(n) }
+	public int getSecondLargest2(int[] arr) {
+		
+		int largest = arr[0];
+		int slargest = Integer.MIN_VALUE;
+		
+		
+		for (int i = 1; i < arr.length; i++) {
+			
+			if(arr[i] > largest) {
+				slargest = largest;	
+				largest = arr[i];
+			}
+			 
+			else if (arr[i] < largest && arr[i] > slargest) {
+				
+				slargest = arr[i];
+				
+			}
+				
+			
+		}
+		
+		
+		return slargest;
+		
+	}
+	
+	
+	
 
 	public static void main(String[] args) {
 		
 		
-		int arr [] = {23,22,4,42,8,29,20};
+		int arr [] = {23,22,4,42,8,29,20,30};
 		
-		System.out.println(new D1_SecondLargestArray().getSecondLargest(arr));
+//		System.out.println(new D1_SecondLargestArray().largest(arr)); //prints 42
 		
+		System.out.println(new D1_SecondLargestArray().getSecondLargest2(arr)); //prints 29
 
 	}
 
